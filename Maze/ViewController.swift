5dc15c22b3765be1b4fcd2973f044221107b960e
oosugiTAKA <taka_kan_tennis@yahoo.co.jp>
2016-02-13 16:51:37 +0900

    一通り完成

--- Maze/ViewController.swift
+++ Maze/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController {
     var playerView: UIView!
     var playerMotionManager: CMMotionManager!//playerを動かすMOtionManager
     //playerが動く速さの宣言
-    var speedX: Double=0.0
-    var speedY: Double=0.0
+    var speedX: Double = 0.0
+    var speedY: Double = 0.0
     
-    let screenSize=UIScreen.mainScreen().bounds.size
-    let maze=[
+    let screenSize = UIScreen.mainScreen().bounds.size
+    let maze = [
         [1,0,0,0,1,0],
         [1,0,1,0,1,0],
         [3,0,1,0,1,0],
@@ -30,8 +30,8 @@ class ViewController: UIViewController {
         [0,1,1,0,0,0],
         [0,0,1,1,1,2],
 ]
-    var goalView:UIView!
-    var startView:UIView!
+    var goalView: UIView!
+    var startView: UIView!
     
     var wallRectArray=[CGRect]()
     
@@ -41,50 +41,51 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let cellWidth=screenSize.width / CGFloat(maze[0].count)//6
-        let cellHeight=screenSize.height / CGFloat(maze.count)//10
+        let cellWidth = screenSize.width / CGFloat(maze[0].count)//6
+        let cellHeight = screenSize.height / CGFloat(maze.count)//10
         
-        let cellOffsetX=screenSize.width / CGFloat(maze[0].count * 2)
-        let cellOffsetY=screenSize.height / CGFloat(maze.count * 2)
+        let cellOffsetX = screenSize.width / CGFloat(maze[0].count * 2)
+        let cellOffsetY = screenSize.height / CGFloat(maze.count * 2)
         
         //for文2つとswich文で画面にオブジェクト配置
-        for y in 0..<maze.count {
-            for x in 0..<maze[y].count{
-                switch maze[y][x]{
+        for y in 0 ..< maze.count {
+            for x in 0 ..< maze[y].count {
+                switch maze[y][x] {
                 case 1:
                     let wallView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX,
                         offsetY: cellOffsetY)
-                    wallView.backgroundColor=UIColor.blackColor()
+                    wallView.backgroundColor = UIColor.blackColor()
                     view.addSubview(wallView)
                     wallRectArray.append(wallView.frame)
                 case 2:
                     startView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX,
                         offsetY: cellOffsetY)
-                    startView.backgroundColor=UIColor.greenColor()
+                    startView.backgroundColor = UIColor.greenColor()
                     self.view.addSubview(startView)
                 case 3:
                     goalView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX,
                         offsetY: cellOffsetY)
-                    goalView.backgroundColor=UIColor.redColor()
+                    goalView.backgroundColor = UIColor.redColor()
                     self.view.addSubview(goalView)
                 default:
                     break
-                
                 }
             }
         }
         
         playerView = UIView(frame: CGRectMake(0 , 8, screenSize.width/60, screenSize.height/60))
-            playerView.center=startView.center
-            playerView.backgroundColor=UIColor.grayColor()
-            self.view.addSubview(playerView)
-            
-            //MotionManager作成
-            playerMotionManager = CMMotionManager()
-            playerMotionManager.accelerometerUpdateInterval=0.02
-            
-            self.startAccelerometer()
+        playerView.center = startView.center
+        playerView.backgroundColor = UIColor.grayColor()
+        self.view.addSubview(playerView)
+        
+        //MotionManager作成
+        playerMotionManager = CMMotionManager()
+        playerMotionManager.accelerometerUpdateInterval=0.02
+        
+        self.startAccelerometer()
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -92,10 +93,11 @@ class ViewController: UIViewController {
     }
 
     //迷路のマスを置きたい場所のステータス(maze配列のどの要素なのか/位置/ますの大きさ)からUIViewを生成するメソッド
-    func createView(x x: Int, y: Int, width: CGFloat, height: CGFloat, offsetX: CGFloat=0, offsetY: CGFloat=0)->
-        UIView{
+    func createView(x x: Int, y: Int, width: CGFloat, height: CGFloat, offsetX: CGFloat = 0, offsetY: CGFloat = 0) ->
+        UIView {
             let rect = CGRect(x: 0, y: 0, width: width, height: height)
             let view = UIView(frame: rect)
+            
             let center = CGPoint(
                 x: offsetX + width * CGFloat(x),
                 y: offsetY + height * CGFloat(y)
@@ -104,60 +106,77 @@ class ViewController: UIViewController {
             view.center = center
             return view
     }
-    
+
+
+
     func startAccelerometer(){
         //加速度取得
-        let handler:CMAccelerometerHandler={(accelerometerData:CMAccelerometerData?,error:NSError?)->Void in
+        let handler:CMAccelerometerHandler = {(accelerometerData:CMAccelerometerData?, error:NSError?) -> Void in
             
             self.speedX += accelerometerData!.acceleration.x
             self.speedY += accelerometerData!.acceleration.y
             
-            var posX=self.playerView.center.x+(CGFloat(self.speedX)/3)
-            var posY=self.playerView.center.y-(CGFloat(self.speedY)/3)
+            var posX=self.playerView.center.x + (CGFloat(self.speedX)/3)
+            var posY=self.playerView.center.y - (CGFloat(self.speedY)/3)
             
-            if posX <= (self.playerView.frame.width/2){
-                self.speedX=0
-                posX=self.playerView.frame.width/2
+            if posX <= (self.playerView.frame.width / 2) {
+                self.speedX = 0
+                posX = self.playerView.frame.width / 2
+            }
+            if posY <= (self.playerView.frame.height / 2) {
+                self.speedY = 0
+                posY = self.playerView.frame.height / 2
+            }
+            if posX >= (self.screenSize.width - (self.playerView.frame.width / 2)) {
+                self.speedX = 0
+                posX = self.screenSize.width - (self.playerView.frame.width / 2)
+            }
+            if posY >= (self.screenSize.height - (self.playerView.frame.height / 2)) {
+                self.speedY = 0
+                posY = self.screenSize.height - (self.playerView.frame.height / 2)
             }
             
-            if posY <= (self.playerView.frame.height/2){
-                self.speedY=0
-                posY=self.playerView.frame.height/2
+            
+            //playerViewとwallViewのあたり判定
+            for wallRect in self.wallRectArray {
+                if (CGRectIntersectsRect(wallRect,self.playerView.frame)){
+                    //                NSLog("Game Over")
+                    self.gameCheck("Game Over", message: "壁に当たりました")
+                    return
+                }
             }
             
-            if posX >= (self.screenSize.width-(self.playerView.frame.width/2)){
-                self.speedX=0
-                posX=self.screenSize.width-(self.playerView.frame.width/2)
-            }
-            
-            if posY >= (self.screenSize.height-(self.playerView.frame.height/2)){
-                self.speedY=0
-                posY=self.screenSize.height-(self.playerView.frame.height/2)
-            }
-            
-            self.playerView.center=CGPointMake(posX , posY)
-            
-        }
-        
-        //playerViewとwallViewのあたり判定
-        for wallRect in self.wallRectArray {
-            if (CGRectIntersectsRect(wallRect,self.playerView.frame)){
-//                NSLog("Game Over")
-                self.gameCheck("Game Over", message: "壁に当たりました")
+            //playerViewとgoalViewのあたり判定
+            if (CGRectIntersectsRect(self.goalView.frame,self.playerView.frame)){
+                //            NSLog("Clear")
+                self.gameCheck("Clear!",message: "クリアしました")
                 return
             }
+
+            self.playerView.center = CGPointMake(posX , posY)
         }
-        
-        //playerViewとgoalViewのあたり判定
-        if (CGRectIntersectsRect(self.goalView.frame,self.playerView.frame)){
-//            NSLog("Clear")
-            self.gameCheck("Clear!",message: "クリアしました")
-            return
-        }
-        
-        //加速度の開始
-        playerMotionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue(),withHandler:handler)
+        playerMotionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: handler)
     }
+    
+//        //playerViewとwallViewのあたり判定
+//        for wallRect in self.wallRectArray {
+//            if (CGRectIntersectsRect(wallRect,self.playerView.frame)){
+////                NSLog("Game Over")
+//                self.gameCheck("Game Over", message: "壁に当たりました")
+//                return
+//            }
+//        }
+//        
+//        //playerViewとgoalViewのあたり判定
+//        if (CGRectIntersectsRect(self.goalView.frame,self.playerView.frame)){
+////            NSLog("Clear")
+//            self.gameCheck("Clear!",message: "クリアしました")
+//            return
+//        }
+//        
+//        //加速度の開始
+//        playerMotionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue(),withHandler:handler)
+    
     
     func gameCheck(result:String,message:String){
         
